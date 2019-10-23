1f5d4fd259883492717f01804ebccaa7ba42c97b
alumno informatica <alumno@ciis.usil.edu.pe>
2019-10-23 08:26:43 -0500

    avance 1

--- Trabajo.py
+++ Trabajo.py
@@ -1,1 +1,34 @@
-a = 2
+print("Bienvenido a Chiquimateando")
+name = input("Hola,Cual es tu nombre : ")
+print("Hola",name) 
+edad = int(input("Cual es tu edad ? : "))
+
+if 7 > edad :
+    print("Lo sentimos eres muy pequeño para poder practicar los temas ")
+if 13 < edad : 
+    print("Este juego ya no esta a tu nivel busca otra manera de practicar")
+
+x = int(input("Hasta que número dominas : "))
+print("Esta bien",name,"procede a elejir un tema que quieras practicar")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
