5d32d98c817282eeb9e566db007f1ccf1e0f74ef
Lucio Tuncar <darktrailml@gmail.com>
2019-10-29 07:50:13 -0500

    i2

--- Interfaz.py
+++ Interfaz.py
@@ -6,4 +6,21 @@ from tkinter import messagebox
 from tkinter import PhotoImage
 
 ventana = Tk()
+#Definimos numeros 
+n = []
 
+#Numero salidos 
+m = []
+
+#Diseño de la Interfaz (Ventana 1)
+ventana.geometry('350x480')
+ventana.title(" CHIQUIMATEANDO")
+etiqueta = Label(ventana, text="Bienvenidos a CHIQUIMATEANDO",bg="#48C9B0")
+etiqueta.pack()
+ventana.config (bg="#48C9B0")
+ttk.Button(ventana, text='Salir', command=ventana.destroy).pack(side=BOTTOM)
+
+#Interfaz Botones 
+
+
+ventana.mainloop()
