4c0f8f03ac9bbc9020fa4833b2501b057421992e
Manuel Rios <manu3554@gmail.com>
2019-10-29 08:29:15 -0500

    impares actualizado

--- Impares.py
+++ Impares.py
@@ -1,1 +1,36 @@
-print()
+import os
+import random
+os.system("cls")
+
+print("Recuerde:")
+print("Un numero es impar si no puede ser dividio entre 2.")
+print("Coloque Si,si desea seguir.")
+print("Coloque No,si desea acabar.")
+
+
+while True:
+    n = random.randint(1,100)
+    p = input("¿El numero "+ str(n) +" es impar?:").lower()
+    if n % 2 != 0:
+        if (p == "si"):
+            print("Correcto.El numero ingresado es impar")
+        if(p == "no"):
+            print("Incorrecto.El numero ingrsado no es impar.")
+    if (n % 2 == 0):
+        if (p == "si"):
+            print("Incorrecto.El numero ingresado no es impar")
+        if(p == "no"):
+            print("Correcto.El numero ingrsado es impar.")
+    
+        
+    ds = input("¿Desea seguir?:").lower()
+    if (ds == "no"):
+        break
+
+
+
+
+
+
+
+
