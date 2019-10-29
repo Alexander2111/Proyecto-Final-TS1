6d8fbb6dc6c471e071e1fc072cb136414a82edd8
Lucio Tuncar <darktrailml@gmail.com>
2019-10-29 08:44:31 -0500

    c4

---
+++ Pares.py
@@ -0,0 +1,28 @@
+import os
+import random
+os.system("cls")
+
+print("Recuerde:")
+print("Un numero es par si se puede ser dividio entre 2.")
+print("Coloque Si,si desea seguir.")
+print("Coloque No,si desea acabar.")
+
+
+while True:
+    n = random.randint(1,100)
+    p = input("¿El numero "+ str(n) +" es impar?:").lower()
+    if n % 2 == 0:
+        if (p == "si"):
+            print("Correcto.El numero ingresado es par")
+        if(p == "no"):
+            print("Incorrecto.El numero ingresado no es par.")
+    if (n % 2 != 0):
+        if (p == "si"):
+            print("Incorrecto.El numero ingresado no es impar")
+        if(p == "no"):
+            print("Correcto.El numero ingrsado no es impar.")
+    
+        
+    ds = input("¿Desea seguir?:").lower()
+    if (ds == "no"):
+        break
