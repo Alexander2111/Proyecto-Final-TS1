fd069443c4a4b4b4f5a1bc126bac86f7efdf383b
Manuel Rios <manu3554@gmail.com>
2019-10-30 07:59:10 -0500

    primos act

--- Primos.py
+++ Primos.py
@@ -2,27 +2,30 @@ import os
 import random
 os.system("cls")
 print("Recuerde:")
-print("Un numero es primo si el numero solo es divisible entre 1 y en el mismo numero.")
+print("Un numero es primo si el numero solo es divisible entre 1 y en el mismo numero.(Solo tiene 2 divisores.)")
 print("Coloque Si , si desea seguir. ")
 print("Coloque No , si desea finalizar. ")
 
 a = 0
 while True:
-    n = random.randint(1,100)
+    n = random.randint(2,100)
     p = input("¿El numero "+ str(n) +" es primo?:").lower()
     for i in range (1,n+1):
         if (n % i == 0):
             a = a + 1
+    
     if(a !=2):
         if (p == "si"):
             print("Incorrecto. No es primo.")
         if (p == "no"):
             print("Correcto.El numero no es primo")
-    if (a==2):
+    if (a == 2):
         if (p == "si"):
             print("Correcto. El numero si es primo.")
         if (p == "no"):
             print("Incorrecto.El numero si es primo")
+    print(n, "Tiene:",a,"divisiores")
+    a = a-a
        
     ds = input("¿Desea seguir?:").lower()
     if (ds == "no"):
